809083a75f0134fbc7b8a8dfe0d944c3d6585313
doppelc <39739292+doppelc@users.noreply.github.com>
2021-03-07 21:25:56 +0100

    Improve code legibility and add SPDX license identifier
    In addition to the legibility changes were:
    - Sharp quote two functions
    - Inline function org-notifications--time-to-hhmm

--- org-notifications.el
+++ org-notifications.el
@@ -6,7 +6,7 @@
 ;; Package-Requires: ((emacs "25.1") (org "9.0") (sound-wav "0.02") (alert "1.2") (seq "2.21"))
 ;; Keywords: outlines
 
-;; License: GNU GPLv3 (or later) <http://www.gnu.org/copyleft/gpl.html>
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;;; Commentary:
 
@@ -31,7 +31,9 @@
 (defcustom org-notifications-non-agenda-file (list "/path/to/file.org")
   "Add a file that's not ordinarily included among your agenda files.
 For example when you want to use an org capture template to create
-notifications without clogging up your org agenda views."
+notifications without clogging up your org agenda views.
+This file is only used if `org-notifications-which-agenda-files'
+is set to use it."
   :group 'org-notifications
   :type 'sexp)
 
@@ -98,7 +100,7 @@ a path to a sound file. E.g. `ding_elevator.wav' or `/path/to/file.wav'"
 (defvar org-notifications--sounds
   '("ding_elevator.wav"
     "ding_percussion.wav")
-   "Packaged sounds.")
+  "Packaged sounds.")
 
 (defvar org-notifications--sounds-directory
   (concat
@@ -116,29 +118,30 @@ Periodically checks if any notification should be created.")
   "Check for agenda events that are nearing and queues notifications for them."
   (save-window-excursion
     (save-restriction
-    (let ((org-agenda-files (org-notifications--agenda-files))
-          (org-agenda-use-time-grid nil)
-          (org-agenda-buffer-name "orgnotifications")
-          (org-agenda-skip-scheduled-if-done 't)
-          (org-agenda-todo-keyword-format "")
-          (org-agenda-remove-tags 't)
-          (org-agenda-window-setup 'current-window)
-          (org-agenda-skip-unavailable-files t)
-          (org-agenda-prefer-last-repeat t)
-          (org-agenda-show-future-repeats nil)
-          (org-agenda-sticky t)
-          (org-agenda-tag-filter-preset org-notifications-agenda-tags-to-ignore)
-          (org-agenda-tag-filter-preset org-notifications-agenda-tags-to-include)
-          (org-agenda-prefix-format '((agenda . "%-19t"))))
-      (org-agenda-list 2))
-    (dolist (times-and-headlines (mapcar 'org-notifications--extract-time-headline
-                       (org-notifications--filter-agenda-no-time
-                        (buffer-substring-no-properties
-                         (point-min)
-                         (point-max)))))
-      (when (org-notifications--entry-within-interval-window (car times-and-headlines))
-          (org-notifications--create-notify (cdr times-and-headlines) (car times-and-headlines))))
-    (org-agenda-exit))))
+      (let ((org-agenda-files (org-notifications--agenda-files))
+            (org-agenda-use-time-grid nil)
+            (org-agenda-buffer-name "orgnotifications")
+            (org-agenda-skip-scheduled-if-done 't)
+            (org-agenda-todo-keyword-format "")
+            (org-agenda-remove-tags 't)
+            (org-agenda-window-setup 'current-window)
+            (org-agenda-skip-unavailable-files t)
+            (org-agenda-prefer-last-repeat t)
+            (org-agenda-show-future-repeats nil)
+            (org-agenda-sticky t)
+            (org-agenda-tag-filter-preset org-notifications-agenda-tags-to-ignore)
+            (org-agenda-tag-filter-preset org-notifications-agenda-tags-to-include)
+            (org-agenda-prefix-format '((agenda . "%-19t"))))
+        (org-agenda-list 2))
+      (dolist (times-and-headlines (mapcar #'org-notifications--extract-time-headline
+                                           (org-notifications--filter-agenda-no-time
+                                            (buffer-substring-no-properties
+                                             (point-min)
+                                             (point-max)))))
+        (when (org-notifications--entry-within-interval-window (car times-and-headlines))
+          (org-notifications--create-notify (cdr times-and-headlines)
+                                            (car times-and-headlines))))
+      (org-agenda-exit))))
 
 (defun org-notifications--agenda-files ()
   "Handle which agenda files should be checked for notifications."
@@ -154,8 +157,11 @@ Notification is set to `org-notifications-notify-before-time' seconds
 before scheduled time.
 HEADLINE is the org agenda entry's headline.
 TIME is the org agenda entry's time."
-  (run-at-time
-   (org-notifications--time-to-hhmm (time-subtract (org-notifications--hhmm-to-time time) org-notifications-notify-before-time)) nil #'org-notifications--notify headline))
+  (run-at-time (format-time-string "%H:%M"
+                                   (time-subtract (org-notifications--hhmm-to-time time)
+                                                  org-notifications-notify-before-time))
+               nil
+               #'org-notifications--notify headline))
 
 (defun org-notifications--notify (headline)
   "Notify with a desktop notification and a sound, if enabled.
@@ -164,7 +170,7 @@ HEADLINE will appear on the notification."
          :title org-notifications-title
          :category "org-notifications")
   (when org-notifications-play-sounds
-      (org-notifications--play-sound)))
+    (org-notifications--play-sound)))
 
 (defun org-notifications--hhmm-to-time (time)
   "Encode TIME such as `17:00' into a time value with today's date."
@@ -173,15 +179,12 @@ HEADLINE will appear on the notification."
           (concat
            (format-time-string "%Y-%m-%d" (current-time)) " " time))))
 
-(defun org-notifications--time-to-hhmm (time)
-  "Format the TIME value to `HH:MM'."
-  (format-time-string "%H:%M" time))
-
 (defun org-notifications--play-sound ()
   "Play a sound from a file (full path) or a prepackaged sound.
 Prepackaged sound comes from `org-notifications--sounds-directory'."
   (if (member org-notifications-sound org-notifications--sounds)
-      (sound-wav-play (concat org-notifications--sounds-directory org-notifications-sound))
+      (sound-wav-play (concat org-notifications--sounds-directory
+                              org-notifications-sound))
     (sound-wav-play org-notifications-sound)))
 
 (defun org-notifications--entry-within-interval-window (time)
@@ -192,20 +195,21 @@ Returns t if entry's time is close enough to create a notification."
                     (org-notifications--hhmm-to-time time)
                     (current-time)))))
     (and
-     (<= time-gap (+ org-notifications-notify-before-time org-notifications-notify-before-time))
+     (<= time-gap (+ org-notifications-notify-before-time
+                     org-notifications-notify-before-time))
      (>= time-gap org-notifications-notify-before-time))))
 
 (defun org-notifications--extract-time-headline (agenda-line-as-string)
   "Create a cons cell containing the time `17:00' and the org agenda headline.
 This is done from AGENDA-LINE-AS-STRING.
- E.g.`TODO Do the dishes' of the agenda entry."
+E.g.`TODO Do the dishes' of the agenda entry."
   (let ((time (car (split-string agenda-line-as-string "        ")))
         (headline (nth 1 (split-string agenda-line-as-string "        "))))
     (cons time headline)))
 
 (defun org-notifications--filter-agenda-no-time (agenda-as-string)
   "Remove items without any time from AGENDA-AS-STRING.
-I.e. items \ without `17:00' and the like."
+I.e. items without `17:00' and the like."
   (seq-filter
    (apply-partially #'string-match "[0-9]\\{0,2\\}:[0-9]\\{2\\}")
    (split-string agenda-as-string "\n")))
@@ -215,7 +219,9 @@ I.e. items \ without `17:00' and the like."
   (interactive)
   (org-notifications--set-style) ;; https://github.com/jwiegley/alert/issues/92
   (setq org-notifications--timer-object
-        (run-at-time 1 org-notifications-notify-before-time 'org-notifications--get-events)))
+        (run-at-time 1
+                     org-notifications-notify-before-time
+                     #'org-notifications--get-events)))
 
 (defun org-notifications-stop ()
   "Stop the timer that is used to collect agenda items."
